a16ab64b89f5a78c9226ed9492405c0abe34c7a5
balsick <balsick@gmail.com>
2017-06-21 12:40:48 +0200

    ciao

--- Lab12/Lab12/Source1.cpp
+++ Lab12/Lab12/Source1.cpp
@@ -15,8 +15,9 @@
 #include <windows.h>
 #include <tchar.h>
 #include <stdio.h>
+#include <cstdlib>
 
-#define BUF_SIZE 51
+#define BUF_SIZE 54
 #define LEN 256
 #define ifmain _tmain
 
@@ -36,64 +37,52 @@ INT ifmain(INT argc, LPTSTR argv[])
 	LARGE_INTEGER filePos;
 	DWORD nWrite, nRead;
 	student stud;
-	TCHAR lpBuffer[BUF_SIZE];
-	OVERLAPPED ov = { 0, 0, 0, 0, NULL }; //Internal, InternalHigh, Offset, OffsetHigh, hEvent
-	OVERLAPPED ov1 = { 0,0,0,0, NULL };
+	TCHAR lpBuffer[BUF_SIZE +1];
+	OVERLAPPED ov1 = { 0,0,0,0, NULL }, ov = { 0, 0, 0, 0, NULL };
 	HANDLE SearchHandle;
 	WIN32_FIND_DATA FindData;
 	TCHAR outName[256];
+	TCHAR oversize[BUF_SIZE];
+	char clpBuffer[BUF_SIZE + 1];
+
+	filePos.QuadPart = 0;
 
 	if (argc != 2) {
 		_ftprintf(stderr, _T("Usage: (%s) database\n"), argv[0]);
 		return 1;
 	}
 
-	while (1)
-	{
-		_tprintf(_T("User choice: |R id| or |W id| or |E|: "));
-		_fgetts(lpBuffer, BUF_SIZE + 1, stdin);
-
-		_tprintf(_T("Your input:\n%s\n"), lpBuffer);
-
-		id = _ttoi(&lpBuffer[2]);
-		switch (lpBuffer[0])
-		{
-		case 'R':
-			SetCurrentDirectory(argv[1]);
-			SearchHandle = FindFirstFile(_T("*"), &FindData);
-			do {
-				_tprintf(_T("%s"), FindData.cFileName);
-				if (_tcscmp(_T("."), FindData.cFileName) == 0 || _tcscmp(_T(".."), FindData.cFileName) == 0)
-					continue;
-				src = CreateFile(FindData.cFileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-				_stprintf(outName, _T("%s.bin"), FindData.cFileName);
-				_tprintf(_T("%s"), outName);
-				hOut = CreateFile(outName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-				while (ReadFile(src, lpBuffer, sizeof(lpBuffer), &nRead, &ov) && nRead > 0) {
-					_stscanf(lpBuffer, _T("%s\t%s\t%s\t%s"), &stud.ip, &stud.dude, &stud.datetime, &stud.length);
-					_tprintf(_T("%s %s %s %s\n"), stud.ip, stud.dude, stud.datetime, stud.length);
-					ov1.Offset = 0xFFFFFFFF;
-					ov1.OffsetHigh = 0xFFFFFFFF;
-					if (WriteFile(hOut, &stud, sizeof(student), &nWrite, &ov1) && nWrite == sizeof(student))
-						_tprintf(_T("Line with id: %s stored correctly.\n"), stud.ip);
-					else
-						_tprintf(_T("Error storing line"));
-				}
-				CloseHandle(hOut);
-				CloseHandle(src);
-			} while (FindNextFile(SearchHandle, &FindData));
-			FindClose(SearchHandle);
-			system("pause");
-			return 1;
-			break; //from case R
-		case 'E':
-			_tprintf(_T("The program will exit...\n"));
-			fflush(stdin);
-			system("pause");
-			return 0;
-		default:
-			_tprintf(_T("Command not supported.\n"));
-			break;
+	SetCurrentDirectory(argv[1]);
+	SearchHandle = FindFirstFile(_T("*.txt"), &FindData);
+	do {
+		filePos.QuadPart = 0;
+		ov.Offset = filePos.LowPart;
+		ov.OffsetHigh = filePos.HighPart;
+		_tprintf(_T("%s"), FindData.cFileName);
+		if (_tcscmp(_T("."), FindData.cFileName) == 0 || _tcscmp(_T(".."), FindData.cFileName) == 0)
+			continue;
+		src = CreateFile(FindData.cFileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		_stprintf(outName, _T("%s.bin"), FindData.cFileName);
+		_tprintf(_T("%s"), outName);
+		hOut = CreateFile(outName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		while (ReadFile(src, clpBuffer, BUF_SIZE -1, &nRead, &ov) && nRead > 0) {
+			mbstowcs(lpBuffer, clpBuffer, BUF_SIZE +1);
+			_stscanf(lpBuffer, _T("%s\t%s\t%s\t%s\r\n%s"), stud.ip, stud.dude, stud.datetime, stud.length, oversize);
+			filePos.QuadPart += _tcslen(stud.ip) + _tcslen(stud.dude) + _tcslen(stud.datetime) + _tcslen(stud.length);
+			ov.Offset = filePos.LowPart;
+			ov.OffsetHigh = filePos.HighPart;
+			_tprintf(_T("Stud data:\n%s %s %s %s\n"), stud.ip, stud.dude, stud.datetime, stud.length);
+			ov1.Offset = 0xFFFFFFFF;
+			ov1.OffsetHigh = 0xFFFFFFFF;
+			if (WriteFile(hOut, &stud, sizeof(student), &nWrite, &ov1) && nWrite == sizeof(student))
+				_tprintf(_T("Line with id: %s stored correctly.\n"), stud.ip);
+			else
+				_tprintf(_T("Error storing line"));
 		}
-	}
+		CloseHandle(hOut);
+		CloseHandle(src);
+	} while (FindNextFile(SearchHandle, &FindData));
+	FindClose(SearchHandle);
+	system("pause");
+	return 1;
 }
